5bf34b1a634135be442615edad6ee303a17f43de
Breno <brenogsbao@gmail.com>
2025-08-29 11:20:09 -0300

    Criando as rotas do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Nav from "./components/Nav"
+import Home from "./routes/Home"
+import Cliente from "./routes/Cliente"
+import Error from "./routes/Error"
+import Footer from "./components/Footer"
 
 
 function App() {
   
 
   return (
-    <>
+    // Habilita a a navegação por rotas
+    <BrowserRouter>
+    <Nav/>
+    <main>
+      <Routes>
+        <Route path="*" element={<Error/>}/>
+        <Route path="/"element={<Home/>}/>
+        <Route path="/cliente"element={<Cliente/>}/>
+
+      </Routes>
+    </main>
+    <Footer/>
       
-    </>
+    </BrowserRouter>
   )
 }
 
